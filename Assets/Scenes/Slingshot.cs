dfab7438ea31418770c64508eb382effe0df5ac4
wilsonbrendan <50500354+wilsonbrendan@users.noreply.github.com>
2020-10-16 10:37:41 -0400

    launchPoint set
    set halo to launchpoint to show when it's being interacted with mouse

---
+++ Assets/Scenes/Slingshot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+    
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        // print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
