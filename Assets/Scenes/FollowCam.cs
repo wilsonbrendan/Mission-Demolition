d1965cb0a8c39a1d290bfe63ce42b19ff587fd42
wilsonbrendan <50500354+wilsonbrendan@users.noreply.github.com>
2020-10-16 13:01:36 -0400

    camera fix
    set camera to be smoother and keep ground in view when following the projectile

---
+++ Assets/Scenes/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+
+    [Header("Set in Header")]
+    public float   easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        //return if no POI
+        if (POI == null) return;
+
+        //get position of POI
+        Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        //set camera to destination
+        transform.position = destination;
+        //set orthographicSize of camera to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
